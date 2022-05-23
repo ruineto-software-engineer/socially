6dde7ad359af42bb7063ec926b00a54ba629cb8c
Rui Neto <ruineto11@gmail.com>
2022-05-23 02:17:21 -0300

    chore: connect to endpoint to load messagens end send messages

--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Swal from "sweetalert2";
 import useApi from "../../hooks/useApi";
 import useAuth from "../../hooks/useAuth";
 import chatbg from "../../assets/backdrops/chatbg.svg";
@@ -24,14 +26,186 @@ import {
   ButtonSend,
   ButtonSendContent,
   ChatContainer,
+  FeedBack,
 } from "./style";
+import { fireAlert } from "../../utils/alerts";
 
 export default function Chat() {
   const { auth, logout } = useAuth();
+  const api = useApi();
   const { recipientId } = useParams();
+  const [status, setStatus] = useState(null);
+  const [recipient, setRecipient] = useState(null);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState(null);
   const navigate = useNavigate();
+  const headers = { headers: { Authorization: `Bearer ${auth?.token}` } };
 
-  console.log("recipientId: ", recipientId);
+  useEffect(() => {
+    handleStatus();
+    handleRecipient();
+    handleMessages();
+  }, []);
+
+  console.log("messages: ", messages);
+
+  const handleKeyDown = async (event) => {
+    if (event.key === "Enter") {
+      handleSendMessage();
+      setMessage("");
+    }
+
+    if (event.key === "Escape") {
+      setMessage("");
+    }
+  };
+
+  async function handleMessages() {
+    try {
+      const { data } = await api.chat.getMessages(
+        auth?.name,
+        parseInt(recipientId),
+        headers
+      );
+
+      setMessages(data);
+    } catch (error) {
+      if (error.response.status === 401) {
+        Swal.fire({
+          title: "Oops...",
+          text: "Your session has expired, please login again to access!",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            handleLogout(auth.userId);
+          }
+        });
+      } else {
+        if (!auth?.token)
+          return fireAlert("You need to be logged in to access!");
+
+        fireAlert(error.response.data);
+      }
+    }
+  }
+
+  async function handleSendMessage() {
+    const messageData = {
+      senderId: auth?.userId,
+      recipientId: parseInt(recipientId),
+      message,
+    };
+
+    try {
+      await api.chat.sendMessage(messageData, headers);
+    } catch (error) {
+      if (error.response.status === 401) {
+        Swal.fire({
+          title: "Oops...",
+          text: "Your session has expired, please login again to access!",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            handleLogout(auth.userId);
+          }
+        });
+      } else {
+        if (!auth?.token)
+          return fireAlert("You need to be logged in to access!");
+
+        fireAlert(error.response.data);
+      }
+    }
+  }
+
+  async function handleStatus() {
+    try {
+      const { data } = await api.chat.getStatusById(recipientId, headers);
+
+      setStatus(data);
+    } catch (error) {
+      if (error.response.status === 401) {
+        Swal.fire({
+          title: "Oops...",
+          text: "Your session has expired, please login again to access!",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            handleLogout(auth.userId);
+          }
+        });
+      } else {
+        if (!auth?.token)
+          return fireAlert("You need to be logged in to access!");
+
+        if (error.response.status === 404) {
+          return fireAlert("User not found!");
+        }
+
+        fireAlert(error.response.data);
+      }
+    }
+  }
+
+  async function handleRecipient() {
+    try {
+      const { data } = await api.user.getUserById(recipientId, headers);
+
+      setRecipient(data);
+    } catch (error) {
+      if (error.response.status === 401) {
+        Swal.fire({
+          title: "Oops...",
+          text: "Your session has expired, please login again to access!",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            handleLogout(auth.userId);
+          }
+        });
+      } else {
+        if (!auth?.token)
+          return fireAlert("You need to be logged in to access!");
+
+        if (error.response.status === 404) {
+          fireAlert(error.response.data);
+          return navigate("/");
+        }
+
+        fireAlert(error.response.data);
+      }
+    }
+  }
+
+  async function handleLogout(userId) {
+    try {
+      await api.auth.logout(userId);
+
+      logout();
+      navigate("/");
+    } catch (error) {
+      fireAlert(error.response.data);
+      navigate("/");
+    }
+  }
+
+  const messagesReader = messages?.map((message) => (
+    <Message
+      key={message.id}
+      message={message.message}
+      recipient={message.senderId !== auth?.userId}
+    />
+  ));
+
+  if (!messages) return "Loading...";
 
   return (
     <div>
@@ -55,35 +229,18 @@ export default function Chat() {
           />
 
           <RecipientDetails>
-            <RecipientName>Charlie Kelly</RecipientName>
-            <RecipientStatus>Online</RecipientStatus>
+            <RecipientName>{recipient?.name}</RecipientName>
+            <RecipientStatus>{status ? "Online" : "Offline"}</RecipientStatus>
           </RecipientDetails>
         </RecipientContainer>
       </ChatBackdropsContainer>
 
       <ChatContainer>
-        <Message recipient message="Oi" />
-        <Message recipient message="Lorem" />
-        <Message
-          recipient={false}
-          message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque purus ut ex dictum pharetra. Integer eget eleifend lacus. Aliquam erat volutpat. Integer dignissim massa leo. Nunc dignissim feugiat molestie. Duis ipsum orci, suscipit eget ullamcorper sit amet, elementum ac massa. Donec sed consectetur nisl. Pellentesque in quam mi. Duis porta facilisis est, eu suscipit nunc laoreet ac. Nulla cursus odio turpis, eget posuere nisi euismod eget. Aliquam erat volutpat."
-        />
-        <Message
-          recipient={false}
-          message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque purus ut ex dictum pharetra. Integer eget eleifend lacus. Aliquam erat volutpat. Integer dignissim massa leo. Nunc dignissim feugiat molestie. Duis ipsum orci, suscipit eget ullamcorper sit amet, elementum ac massa. Donec sed consectetur nisl. Pellentesque in quam mi. Duis porta facilisis est, eu suscipit nunc laoreet ac. Nulla cursus odio turpis, eget posuere nisi euismod eget. Aliquam erat volutpat."
-        />
-        <Message
-          recipient={false}
-          message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque purus ut ex dictum pharetra. Integer eget eleifend lacus. Aliquam erat volutpat. Integer dignissim massa leo. Nunc dignissim feugiat molestie. Duis ipsum orci, suscipit eget ullamcorper sit amet, elementum ac massa. Donec sed consectetur nisl. Pellentesque in quam mi. Duis porta facilisis est, eu suscipit nunc laoreet ac. Nulla cursus odio turpis, eget posuere nisi euismod eget. Aliquam erat volutpat."
-        />
-        <Message
-          recipient
-          message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque purus ut ex dictum pharetra. Integer eget eleifend lacus. Aliquam erat volutpat. Integer dignissim massa leo. Nunc dignissim feugiat molestie. Duis ipsum orci, suscipit eget ullamcorper sit amet, elementum ac massa. Donec sed consectetur nisl. Pellentesque in quam mi. Duis porta facilisis est, eu suscipit nunc laoreet ac. Nulla cursus odio turpis, eget posuere nisi euismod eget. Aliquam erat volutpat."
-        />
-        <Message
-          recipient
-          message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pellentesque purus ut ex dictum pharetra. Integer eget eleifend lacus. Aliquam erat volutpat. Integer dignissim massa leo. Nunc dignissim feugiat molestie. Duis ipsum orci, suscipit eget ullamcorper sit amet, elementum ac massa. Donec sed consectetur nisl. Pellentesque in quam mi. Duis porta facilisis est, eu suscipit nunc laoreet ac. Nulla cursus odio turpis, eget posuere nisi euismod eget. Aliquam erat volutpat."
-        />
+        {!messagesReader?.length ? (
+          <FeedBack>Send a "Hello" to your contact!</FeedBack>
+        ) : (
+          messagesReader
+        )}
       </ChatContainer>
 
       <InputControlContainer>
@@ -92,7 +249,12 @@ export default function Chat() {
           <ButtonSend alt="buttonSend.svg" src={buttonSend} />
         </ButtonSendContent>
 
-        <InputControl placeholder="Write a message..." />
+        <InputControl
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={handleKeyDown}
+          placeholder="Write a message..."
+        />
       </InputControlContainer>
     </div>
   );
--- src/services/ChatApi.js
+++ src/services/ChatApi.js
@@ -2,10 +2,25 @@ import api from "./api";
 
 export default class ChatApi {
   getAllContacts(userId, headers) {
-    return api.get(`/chat/${userId}`, headers);
+    return api.get(`/chats/${userId}`, headers);
   }
 
   getContactsByName(userId, userName, headers) {
-    return api.get(`/chat/search/${userId}?userName=${userName}`, headers);
+    return api.get(`/chats/search/${userId}?userName=${userName}`, headers);
+  }
+
+  getStatusById(userId, headers) {
+    return api.get(`/chat/status/${userId}`, headers);
+  }
+
+  sendMessage(messageData, headers) {
+    return api.post("/chat/send", messageData, headers);
+  }
+
+  getMessages(senderName, recipientId, headers) {
+    return api.get(
+      `/chat/messages/${recipientId}?senderName=${senderName}`,
+      headers
+    );
   }
 }
